d007090003693ad492b6796bd36dfc9d6c8613ac
rodrigoanunziato <rodrigoanunziato@hotmail.com>
2024-01-23 23:28:22 -0300

    Update seller and custom service exception

--- SalesWeb/Models/ViewModels/SellerFormViewModel.cs
+++ SalesWeb/Models/ViewModels/SellerFormViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace SalesWeb.Models.ViewModels
 {
-    public class SellerForViewModel
+    public class SellerFormViewModel
     {
         public Seller Seller { get; set; }
         public ICollection<Departments> Departments { get; set; }
--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using SalesWeb.Data;
 using SalesWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesWeb.Services.Exceptions;
 
 namespace SalesWeb.Services
 {
@@ -34,6 +35,23 @@ namespace SalesWeb.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+        
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
